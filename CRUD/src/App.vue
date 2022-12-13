4f9fe1c986337eacb64778778d3fd81475f7768a
ZamRom <zamromxd@gmail.com>
2022-12-13 01:52:32 -0600

    con propiedad computada

--- CRUD/src/App.vue
+++ CRUD/src/App.vue
@@ -1,36 +1,33 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 const contador = ref(0)
 const aumentar = () => {
   contador.value++
-  color()
 }
 const disminuir = () => {
   contador.value -= 1
-  color()
 }
-const color = () => {
+const color = computed( ()=> {
   switch (true) {
+    case contador.value === 0:
+      return ''
+      break;
     case contador.value < 0:
-      aplicarClase.value = 'rojo'
-      break;
+      return 'rojo'
+      break
     case contador.value > 0:
-      aplicarClase.value = 'verde'
-      break;
-    default:
-      aplicarClase.value = ''
+      return 'verde'
       break;
   }
-}
+})
 const reseteo = () => {
   contador.value = 0
-  aplicarClase.value = ''
 }
 const aplicarClase = ref('')
 </script>
 
 <template>
-  <h1>contador <strong :class="aplicarClase">{{ contador }}</strong></h1><br>
+  <h1>contador <strong :class="color">{{ contador }}</strong></h1><br>
   <button @click="aumentar">aumentar</button>
   <button @click="disminuir">disminuir</button>
   <button @click="reseteo">resetear</button>
