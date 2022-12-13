da0922e7562c932e838b2706b029f94fd3be37eb
ZamRom <zamromxd@gmail.com>
2022-12-13 01:40:40 -0600

    contador con clases

--- CRUD/src/App.vue
+++ CRUD/src/App.vue
@@ -1,44 +1,50 @@
 <script setup>
 import { ref } from 'vue';
-const name = 'crud';
-const azul = "color:aqua";
-const arrayColores = ['blue', 'red', 'aqua'];
-const activo = ref(false);
-const pokemones = ref(undefined);
-const click1 = () => {
-  activo.value = !activo.value
-  alert(`El estado actual es ${activo.value}`)
-};
-const traePokemones = () => {
-  fetch('https://pokeapi.co/api/v2/pokemon/')
-    .then(res => res.json())
-    .then(data => {
-      let pruebas = []
-      data.results.forEach((element) => {
-        pruebas.push(element);
-      });
-      pokemones.value = pruebas
-    })
-    .catch(error => console.log(error))
+const contador = ref(0)
+const aumentar = () => {
+  contador.value++
+  switch (true) {
+    case contador.value < 0:
+      aplicarClase.value = 'rojo'
+      break;
+    case contador.value > 0:
+      aplicarClase.value = 'verde'
+      break;
+    default:
+      aplicarClase.value = ''
+      break;
+  }
 }
-const borrarPokemones = () => {
-  pokemones.value = undefined
+const disminuir = () => {
+  contador.value -= 1
+  switch (true) {
+    case contador.value < 0:
+      aplicarClase.value = 'rojo'
+      break;
+    case contador.value > 0:
+      aplicarClase.value = 'verde'
+      break;
+    default:
+      aplicarClase.value = ''
+      break;
+  }
 }
+
+const aplicarClase = ref('')
 </script>
 
 <template>
-  <h1>Hola {{ name.toUpperCase() }}</h1> <br>
-  <button @click="traePokemones">Trae Pokemones</button><br>
-  <button @click="borrarPokemones">Borrar Pokemones</button><br>
-  <div v-if="pokemones">
-    <ul>
-      <li v-for="poke in pokemones">{{ poke.name }}</li>
-    </ul>
-  </div>
+  <h1>contador <strong :class="aplicarClase">{{ contador }}</strong></h1><br>
+  <button @click="aumentar">aumentar</button>
+  <button @click="disminuir">disminuir</button>
 </template>
 
 <style>
-h1 {
+.verde {
+  color: aqua;
+}
+
+.rojo {
   color: red;
 }
 </style>
